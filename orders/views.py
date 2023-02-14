01bf89da105c4238b4c558c3a6b74879cbeb4352
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2023-02-14 18:18:22 +0800

    Create Order Flash Message

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseForbidden
+from django.contrib import messages
 from cart.cart import Cart
 from .models import Order, OrderItem
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='login_wholesaler')
 def display_orders(request):
+    try:
+        request.user.wholesaler
+    except:
+        return HttpResponseForbidden()
+    
     wholesaler = request.user.wholesaler
     orders = wholesaler.order_set.all()
     context = {'orders':orders}
     return render(request, 'orders/orders.html', context)
 
 
+@login_required(login_url='login_retailer')
 def create_order(request):
+    try:
+        request.user.retailer
+    except:
+        return HttpResponseForbidden()
+    
     cart = Cart(request)
     order_status = ''
     retailer = request.user.retailer
@@ -38,4 +54,5 @@ def create_order(request):
     
     cart.clear()
     request.user.cart_db_set.filter(user=request.user).delete()
+    messages.success(request, 'Your Order is successfully placed!')
     return redirect('show_shop')
