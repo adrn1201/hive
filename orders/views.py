4c94a8ffbbdc3094aac96951e73b330815055981
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2023-02-20 15:44:35 +0800

    Updates order pending

--- orders/views.py
+++ orders/views.py
@@ -50,15 +50,6 @@ def create_order(request):
             price=item['price'], 
             quantity=item['qty']
         )
-        inventory = Inventory.objects.get(id=order_item.product.id)
-        category = Category.objects.get(id=inventory.category.id)
-        category.sold += int(order_item.quantity)
-        inventory.sold += int(order_item.quantity)
-        inventory.actual_quantity -= int(order_item.quantity)
-        if inventory.actual_quantity < 0:
-            inventory.actual_quantity = 0
-        category.save()
-        inventory.save()
     
     cart.clear()
     request.user.cart_db_set.filter(user=request.user).delete()
@@ -79,10 +70,25 @@ def order_details(request, pk):
     
     if request.method == "POST":
         form = OrderForm(request.POST, instance=order)
+
         if form.is_valid():
-            form.save()
+            order_form = form.save(commit=False)
+            if order_form.status == 'pending':
+                for item in order_items:
+                    inventory = Inventory.objects.get(id=item.product.id)
+                    category = Category.objects.get(id=inventory.category.id)
+                    category.sold += int(item.quantity)
+                    inventory.sold += int(item.quantity)
+                    inventory.actual_quantity -= int(item.quantity)
+                    if inventory.actual_quantity < 0:
+                        inventory.actual_quantity = 0
+                    category.save()
+                    inventory.save()
+                    
+            order_form.save()
             messages.success(request, 'Order status successfully updated!')
             return redirect('order_details', pk=order.id)
+
     
     context = {'order':order, 'order_items':order_items, 'form':form}
     return render(request, 'orders/order_details.html', context)    
