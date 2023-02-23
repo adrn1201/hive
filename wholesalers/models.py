60316bd01e27d13c32d77ed6f3a51355663d8e58
unknown <darrenjustin.wu@benilde.edu.ph>
2023-02-23 15:12:26 +0800

    Fixed profile picture and search bar in shop

--- wholesalers/models.py
+++ wholesalers/models.py
@@ -9,6 +9,7 @@ class Wholesaler(TenantMixin):
     contact_name = models.CharField(max_length=255)
     contact_number = models.CharField(max_length=50)
     is_active = models.BooleanField(default=False)
+    wholesaler_image = models.ImageField(default='products/default.jpg', upload_to="products/")
     created = models.DateTimeField(auto_now_add=True)
     
     auto_create_schema = True
