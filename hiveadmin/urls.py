102bfeb8bae65a8bfded8191f38da9b5c6b28f3e
unknown <darrenjustin.wu@benilde.edu.ph>
2023-03-01 19:17:07 +0800

    backend

--- hiveadmin/urls.py
+++ hiveadmin/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.list_wholesalers, name="list_wholesalers"),
+    path('register/', views.register_admin, name='register_admin'),
+    path('login/', views.login_admin, name='login_admin'),
     path('<int:pk>', views.update_wholesaler, name='update_wholesaler'),
-    path('inactive/', views.list_deac, name="list_deac"),
+    path('logout/', views.logout_admin, name="logout_admin"),
 
 ]
--- hiveadmin/views.py
+++ hiveadmin/views.py
@@ -1,14 +1,72 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseForbidden
 from wholesalers.models import Wholesaler
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib import messages
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from .utils import login_user, logout_user, search_products
+
+@login_required(login_url='login_admin')
+def register_admin(request):
+
+    if request.user.is_authenticated and request.user.is_superuser:
+        pass
+    elif request.user.is_authenticated and not request.user.is_superuser:
+        return redirect('login_admin')
+    
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
+            global user_credentials
+            user_credentials = user
+            return redirect('register_admin')
+        else:
+            pass
+        
+
+    context = {'form': form}
+
+    return render(request, "hiveadmin/create_admin.html", context)
 
 
+def login_admin(request):
+    '''
+    This function is for wholesaler account authentication
+    '''
+
+    return login_user(request, 'list_wholesalers', 'hiveadmin/admin_login.html')
+
+
+@login_required(login_url='login_admin')
+def logout_admin(request):
+    return logout_user(request, 'login_admin')
+
+# ----------------------------------------------------------------
+
+@login_required(login_url='login_admin')
 def list_wholesalers(request):
+     
+    if request.user.is_authenticated and request.user.is_superuser:
+        pass
+    elif request.user.is_authenticated and not request.user.is_superuser:
+        return redirect('login_admin')
+    
+    wholesaler = Wholesaler.objects.get()
+    categories = wholesaler.category_set.all()
+    wholesalers, search_query = search_products(request)
+
 
     wholesalers = Wholesaler.objects.exclude(id=1).filter(is_active=True)
-    context = {'wholesalers': wholesalers}
+    context = {'wholesalers': wholesalers, 'categories':categories, 'search_query': search_query}
 
     if(request.method == "POST"):
         send_mail(
@@ -23,14 +81,13 @@ def list_wholesalers(request):
     return render(request, 'hiveadmin/wholesalers_list.html',context)
 
 
-def list_deac(request):
-
-    wholesalers = Wholesaler.objects.filter(is_active=False)
-    context = {'wholesalers': wholesalers}
-    return render(request, 'hiveadmin/wholesalers_deac.html',context)
-
-
+@login_required(login_url='login_admin')
 def update_wholesaler(request, pk):
+
+    if request.user.is_authenticated and request.user.is_superuser:
+        pass
+    elif request.user.is_authenticated and not request.user.is_superuser:
+        return redirect('login_admin')
     
     wholesaler = Wholesaler.objects.get(id=pk)
     
