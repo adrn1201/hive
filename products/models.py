539f2677793fe306ac5b05b76fa75142f4162037
unknown <darrenjustin.wu@benilde.edu.ph>
2023-03-08 15:14:07 +0800

    fixed

--- products/models.py
+++ products/models.py
@@ -41,6 +41,7 @@ class Product(models.Model):
     
     class Meta:
         verbose_name_plural = 'Products'
+        ordering = ['-created']
          
     def __str__(self):
         return self.product_name
